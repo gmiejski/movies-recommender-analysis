be65b2c30bed6c06f504ae83cb5c1353c53246ad
gmiejski <grz.miejski@gmail.com>
2016-05-11 20:31:30 +0200

    ratings throughput

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # movies-recommender-analysis
+
+Calculating max ratings per second (ratings_throughput.py):
+
+[('825638400', 643), ('1267135219', 207), ('1122405887', 106)]
---
+++ ratings_throughput.py
@@ -0,0 +1,8 @@
+import collections
+
+# with open("../resources/ratings.csv") as f:
+with open("resources/ratings.csv") as f:
+    lines = f.readlines()
+    a = map(lambda line: line.split(",")[3], lines)
+    counter=collections.Counter(a)
+    print(counter.most_common(3))
